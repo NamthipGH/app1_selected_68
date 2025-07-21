2bdb288365708a8447f5bd88b69df9b92df5ac62
NamthipGH <156067738+NamthipGH@users.noreply.github.com>
2025-07-21 10:55:41 +0700

    function component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+
 import React from 'react'
-import'./style.css'
+import { Content, Header } from './func-components';
+
 
 function App() {
 
   return (
-    <>
-    <h2>DeveloperThai.com</h2>
-    <div className='title'>React & React Native</div>
-    </>
+    [<Header/>,<Content/>,<Footer/>
+    ]
   )
 }
 
